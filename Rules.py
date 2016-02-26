59a013852e1945930b8a6452b9f85413bdf9104a
Yitong <zha.yitong@gmail.com>
2016-02-26 01:44:01 -0500

    outline basic rules in comments
    also upload rulebook pdf

---
+++ Rules.py
@@ -0,0 +1,24 @@
+# decklist = [villager, villager, villager, werewolf, werewolf, seer, robber, troublemaker, tanner, drunk, hunter, mason, mason, insomniac, minion, doppelganger]
+
+# villager Just some regular person.
+# werewolf: At night, opens their eyes and recognize each other. There is no killing.
+# seer: At night, looks at 1 player card or 2 center cards.
+# robber: At night, can switch his card with another player if he wishes. If he does, he can look at his new card.
+# troublemaker: At night, can switch the cards of two other players. She does not look at the cards.
+# tanner: Wins if he dies, making both villagers and werewolves lose. If tanner & a werewolf both die, villagers win alongside tanner.
+# drunk: At night, must exchange card with a card in the center. He does not look at his new card.
+# hunter: If hunter dies, the player he voted for dies as well.
+# mason: At night, wakes up and recognizes the other mason.
+# insomniac: At night, wakes up and looks at her own card.
+# minion: At night, is shown who the werewolves are but the werewolves don't know who the minion is. If the minion dies and no werewolves die, the werewolves & minion win. 
+# doppelganger: At night, looks at a player's card and immediately becomes that role.
+
+# game phase order: distribute role cards, night, day debates, cast vote for player to kill, game over
+
+# night order: doppelganger, werewolves, minion, mason, seer, robber, troublemaker, drunk. insomniac
+
+# role card distribution: 
+# 3 players = [villager, werewolf, werewolf, seer, robber, troublemaker]
+# 4 players = [villager, villager, werewolf, werewolf, seer, robber, troublemaker]
+# 5 players = [villager, villager, villager, werewolf, werewolf, seer, robber, troublemaker]
+# 5+ players: players decide what roles they want to add
